342a7f43d1d80e3579e9eabedac4fcd17749bc5c
Flarxx <flarx3@gmail.com>
2025-08-05 00:54:00 -0400

    Implemente navbar  + tarjetas para presentar mi experiencia

--- nucleo/views.py
+++ nucleo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'nucleo/layout.html')
+
+
+def inicio(request):
+    return render(request, 'nucleo/main.html')
