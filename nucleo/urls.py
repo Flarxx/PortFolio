b572c2ed3c8e0eb21159cf5bc4ad9761c31da844
flarxx <flarx3@gmail.com>
2025-12-11 12:31:17 -0400

    probando

--- nucleo/urls.py
+++ nucleo/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('', views.inicio, name='home'),
     path('proyectos/', views.proyectos, name='proyectos'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('certificados/', views.certificados, name='certificados'),
 ]
--- nucleo/views.py
+++ nucleo/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
+from .models import Project
+from .models import Certificate, ContactMessage
+from .forms import ContactForm
+from django.contrib import messages
+from django.shortcuts import redirect
+
 
 def inicio(request):
     return render(request, 'nucleo/main.html')
 
+
 def proyectos(request):
-    return render(request, 'nucleo/proyectos.html')
+    projects = Project.objects.all()
+    return render(request, 'nucleo/Proyectos.html', {'projects': projects})
+
+
+def certificados(request):
+    certs = Certificate.objects.all()
+    return render(request, 'nucleo/certificados.html', {'certificates': certs})
+
+
+def contacto(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Mensaje enviado. Gracias!')
+            return redirect('contacto')
+    else:
+        form = ContactForm()
+    return render(request, 'nucleo/contact.html', {'form': form})
 
